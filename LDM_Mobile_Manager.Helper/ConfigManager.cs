32ae8f70e66e17652adebabf34488238e1b7b330
ReemAli41 <reem.ali.abdelrahman@gmail.com>
2025-05-18 17:06:55 +0300

    Helpes Layer

--- LDM_Mobile_Manager.Helper/ConfigManager.cs
+++ LDM_Mobile_Manager.Helper/ConfigManager.cs
@@ -1,7 +1,15 @@
-﻿namespace LDM_Mobile_Manager.Helper
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDM_Mobile_Manager.Helper
 {
     public class ConfigManager
     {
-
+        public string ClientCode { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
