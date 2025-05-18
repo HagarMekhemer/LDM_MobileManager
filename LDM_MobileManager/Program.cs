5b8be1caab627849e55993b2b55e283a816e4bf2
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-18 17:07:41 +0300

    //Updated files

--- LDM_MobileManager/Program.cs
+++ LDM_MobileManager/Program.cs
@@ -1,12 +1,27 @@
+using LDM_Mobile_Manager.DSL;
+using LDM_Mobile_Manager.LDM_Mobile_Manager.Helper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Inject custom services
+builder.Services.AddSingleton<TokenManager>();
+builder.Services.AddScoped<TokenDSL>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
--- LDM_Mobile_Manager.DataService/TokenDSL.cs
+++ LDM_Mobile_Manager.DataService/TokenDSL.cs
@@ -1,7 +1,27 @@
-﻿namespace LDM_Mobile_Manager.DataService
+﻿using LDM_Mobile_Manager.Common.Entities.RequestDTOs;
+using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using LDM_Mobile_Manager.LDM_Mobile_Manager.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LDM_Mobile_Manager.DSL
 {
     public class TokenDSL
     {
+        private readonly TokenManager _tokenManager;
 
+        public TokenDSL(TokenManager tokenManager)
+        {
+            _tokenManager = tokenManager;
+        }
+
+        public GenerateTokenResponseDTO GenerateToken(TokenCredentialsRequestDTO request)
+        {
+            return _tokenManager.GetToken(request);
+        }
     }
 }
