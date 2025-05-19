52dbc997daad1a096c347e83d82f5fc0efb6c0d6
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-19 11:01:07 +0300

    //Updated with swagger

--- LDM_MobileManager/Program.cs
+++ LDM_MobileManager/Program.cs
@@ -1,6 +1,6 @@
 using LDM_Mobile_Manager.DSL;
 using LDM_Mobile_Manager.LDM_Mobile_Manager.Helper;
-//using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 // Inject custom services
 builder.Services.AddSingleton<TokenManager>();
@@ -17,12 +17,12 @@ builder.Services.AddScoped<TokenDSL>();
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwagger();
-//}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
