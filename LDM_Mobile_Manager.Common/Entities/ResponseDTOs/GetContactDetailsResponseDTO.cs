fab1cef434c68fc0b9ba02aee1037bf221b81445
ReemAli41 <110223655+ReemAli41@users.noreply.github.com>
2025-05-20 17:44:50 +0300

    Add files via upload
    Add Some DTOs

---
+++ LDM_Mobile_Manager.Common/Entities/ResponseDTOs/GetContactDetailsResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace LDM_Mobile_Manager.Common.Entities.ResponseDTOs
+{
+    public class GetContactDetailsResponseDTO
+    {
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public string Contact { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+    }
+}
---
+++ LDM_Mobile_Manager.Common/Entities/ResponseDTOs/GetScientificNewsResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace LDM_Mobile_Manager.Common.Entities.ResponseDTOs
+{
+    public class GetScientificNewsResponseDTO
+    {
+        [Required]
+        public string ArabicTitle { get; set; }
+
+        [Required]
+        public string EnglishTitle { get; set; }
+
+        public string ArabicContentText { get; set; }
+
+        public string EnglishContentText { get; set; }
+    }
+}
