b9c1c9b7070a94334e3cd08577156bb4ec7a26ae
ReemAli41 <110223655+ReemAli41@users.noreply.github.com>
2025-05-20 17:38:18 +0300

    Add files via upload

---
+++ LDM_Mobile_Manager.Common/Entities/ResponseDTOs/GetBookResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace LDM_Mobile_Manager.Common.Entities.ResponseDTOs
+{
+    public class GetBookResponseDTO
+    {
+        [Required]
+        public string ArabicTitle { get; set; }
+
+        [Required]
+        public string EnglishTitle { get; set; }
+
+        [Required]
+        public string ContentAttachment { get; set; }
+    }
+}
+
