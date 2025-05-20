2322d5f4cb55980fe2c4aae958c7daddc2c042b9
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-20 14:12:12 +0300

    // Mobile user profile added

---
+++ LDM_MobileManager/Controllers/MobileUserProfileController.cs
@@ -0,0 +1,25 @@
+﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using LDM_Mobile_Manager.DataService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDM_MobileManager.Controllers
+{
+        [ApiController]
+        [Route("NT/Data")]
+        public class MobileUserProfileController : ControllerBase
+        {
+            private readonly MobileUserProfileDSL _dsl;
+
+            public MobileUserProfileController(MobileUserProfileDSL dsl)
+            {
+                _dsl = dsl;
+            }
+
+            [HttpGet("GetMobileUserProfile")]
+            public async Task<ActionResult<ResponseDTO<List<GetMobileUserProfileResponseDTO>>>> GetMobileUserProfile()
+            {
+                var data = await _dsl.GetMobileUserProfile();
+                return Ok(new ResponseDTO<List<GetMobileUserProfileResponseDTO>>(true, "Request Processed Successfully", data));
+            }
+        }
+}
---
+++ LDM_Mobile_Manager.Repo/MobileUserProfileRepo.cs
@@ -0,0 +1,27 @@
+﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDM_Mobile_Manager.Repo
+{
+    public class MobileUserProfileRepo
+    {
+        public async Task<List<GetMobileUserProfileResponseDTO>> GetMobileUserProfile()
+        {
+            var profiles = new List<GetMobileUserProfileResponseDTO>
+            {
+                new GetMobileUserProfileResponseDTO
+                {
+                    ProfileName = "Patient",
+                    ActivePages = new List<string> { "Services" }
+                }
+            };
+
+            return await Task.FromResult(profiles);
+        }
+    }
+}
+    
