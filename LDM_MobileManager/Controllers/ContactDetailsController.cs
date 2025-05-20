6c7c7e939ebf9928fe94700892ed67a088b9abd6
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-20 11:25:19 +0300

    //GetContactDetails Added

---
+++ LDM_MobileManager/Controllers/ContactDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using LDM_Mobile_Manager.DataService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDM_MobileManager.Controllers
+{
+    [ApiController]
+    [Route("NT/Data")]
+    public class ContactDetailsController : ControllerBase
+    {
+        private readonly ContactDetailsDSL _dsl;
+
+        public ContactDetailsController(ContactDetailsDSL dsl)
+        {
+            _dsl = dsl;
+        }
+
+        [HttpGet("GetContactDetails")]
+        public async Task<ActionResult<ResponseDTO<List<GetContactDetailsResponseDTO>>>> GetContactDetails()
+        {
+            var data = await _dsl.GetContactDetails();
+            return Ok(new ResponseDTO<List<GetContactDetailsResponseDTO>>(true, "Request Processed Successfully", data));
+        }
+    }
+}
---
+++ LDM_Mobile_Manager.DataService/ContactDetailsDSL.cs
@@ -0,0 +1,25 @@
+﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using LDM_Mobile_Manager.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDM_Mobile_Manager.DataService
+{
+    public class ContactDetailsDSL
+    {
+        private readonly ContactDetailsRepo _repo;
+
+        public ContactDetailsDSL(ContactDetailsRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<GetContactDetailsResponseDTO>> GetContactDetails()
+        {
+            return await _repo.GetContactDetails();
+        }
+    }
+}
