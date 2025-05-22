4ddfa23e322ed1aa7ac649bd2ebc0e3e2d974581
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-22 10:03:50 +0300

    //Added HandleMiddleWare

--- LDM_MobileManager/Program.cs
+++ LDM_MobileManager/Program.cs
@@ -11,51 +11,7 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//builder.Services.AddScoped<IBaseRepo<GetNewsDataResponseDTO>,NewsDataRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetPromotionResponseDTO>, PromotionRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetBrochuresResponseDTO>, BrochureRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetPressResponseDTO>, PressRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetBranchesResponseDTO>, BranchRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetFeedbackMailResponseDTO>, FeedbackMailRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetLabServicesResponseDTO>, LabServicesRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetAboutLabDetailsResponseDTO>, AboutLabDetailsRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetBookResponseDTO>, BooksRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetClientDataVersionResponseDTO>, ClientDataVersionRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetContactDetailsResponseDTO>, ContactDetailsRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetGalleryImageResponseDTO>, GalleryRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetHealthTipResponseDTO>, HealthTipsRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetMobileUserProfileResponseDTO>, MobileUserProfileRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetNewsletterResponseDTO>, NewslettersRepo>();
-//builder.Services.AddScoped<IBaseRepo<GetScientificNewsResponseDTO>, ScientificNewsRepo>();
-
-void RegisterAssemblies()
-{
-    // Load all relevant assemblies
-    var assemblies = new[] {
-        "LDM_Mobile_Manager.Repo"
-    }.Select(Assembly.Load).ToList();
-
-    // Register all IBaseRepo<T> implementations with their interface
-    foreach (var type in assemblies.SelectMany(a => a.GetTypes()))
-    {
-        if (type.Name.EndsWith("Repo") && !type.IsInterface && !type.IsAbstract)
-        {
-            var repoInterface = type.GetInterfaces()
-                .FirstOrDefault(i => i.IsGenericType &&
-                    i.GetGenericTypeDefinition() == typeof(IBaseRepo<>));
-
-            if (repoInterface != null)
-            {
-                builder.Services.AddScoped(repoInterface, type);
-            }
-        }
-    }
-}
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -80,6 +36,32 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
 app.MapControllers();
 
 app.Run();
+
+void RegisterAssemblies()
+{
+    // Load all relevant assemblies
+    var assemblies = new[] {
+        "LDM_Mobile_Manager.Repo"
+    }.Select(Assembly.Load).ToList();
+
+    // Register all IBaseRepo<T> implementations with their interface
+    foreach (var type in assemblies.SelectMany(a => a.GetTypes()))
+    {
+        if (type.Name.EndsWith("Repo") && !type.IsInterface && !type.IsAbstract)
+        {
+            var repoInterface = type.GetInterfaces()
+                .FirstOrDefault(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IBaseRepo<>));
+
+            if (repoInterface != null)
+            {
+                builder.Services.AddScoped(repoInterface, type);
+            }
+        }
+    }
+}
---
+++ LDM_Mobile_Manager.Helper/ErrorHandlerMiddleware.cs
@@ -0,0 +1,87 @@
+﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandlerMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger, IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception occurred");
+            await HandleExceptionAsync(context, ex, _env);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception, IHostEnvironment env)
+    {
+        context.Response.ContentType = "application/json";
+
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        string message;
+
+        switch (exception)
+        {
+            case UnauthorizedAccessException:
+                statusCode = HttpStatusCode.Unauthorized;
+                message = "Unauthorized access";
+                break;
+
+            case ArgumentNullException:
+            case ArgumentException:
+                statusCode = HttpStatusCode.BadRequest;
+                message = exception.Message;
+                break;
+
+            case NotImplementedException:
+                statusCode = HttpStatusCode.NotImplemented;
+                message = exception.Message;
+                break;
+
+            default:
+                statusCode = HttpStatusCode.InternalServerError;
+                message = "Please re-try again, if the error persist please contact Administrator";
+                break;
+        }
+
+        // Show full error details in development
+        if (env.IsDevelopment())
+        {
+            message += $" | Exception: {exception.Message} | StackTrace: {exception.StackTrace}";
+        }
+
+        context.Response.StatusCode = (int)statusCode;
+
+        var response = new ResponseDTO<object>(
+            false,
+            message,
+            null
+        );
+
+        var result = JsonSerializer.Serialize(response);
+        return context.Response.WriteAsync(result);
+    }
+}
