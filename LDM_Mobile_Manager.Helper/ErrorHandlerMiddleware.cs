2e722809d56e97ac874c278d1f6c7033c80224ff
ReemAli41 <reem.ali.abdelrahman@gmail.com>
2025-05-22 11:25:10 +0300

    //Update Exception Middleware

--- LDM_Mobile_Manager.Helper/ErrorHandlerMiddleware.cs
+++ LDM_Mobile_Manager.Helper/ErrorHandlerMiddleware.cs
@@ -1,12 +1,10 @@
-﻿using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+﻿using LDM_Mobile_Manager.Common.Entities.Exceptions;
+using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -36,40 +34,51 @@ public class ErrorHandlerMiddleware
         }
     }
 
-    private static Task HandleExceptionAsync(HttpContext context, Exception exception, IHostEnvironment env)
+    private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
     {
         context.Response.ContentType = "application/json";
 
-        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        HttpStatusCode statusCode;
         string message;
 
-        switch (exception)
+        switch (ex)
         {
-            case UnauthorizedAccessException:
+            case UnauthorizedException:
                 statusCode = HttpStatusCode.Unauthorized;
-                message = "Unauthorized access";
+                message = ex.Message;
                 break;
 
             case ArgumentNullException:
             case ArgumentException:
                 statusCode = HttpStatusCode.BadRequest;
-                message = exception.Message;
+                message = ex.Message;
                 break;
 
             case NotImplementedException:
                 statusCode = HttpStatusCode.NotImplemented;
-                message = exception.Message;
+                message = ex.Message;
+                break;
+
+            case NoContentException:
+                statusCode = HttpStatusCode.NoContent;
+                message = ex.Message;
+                break;
+
+            case CustomValidationException:
+            case CustomResponseValidationException:
+                statusCode = HttpStatusCode.BadRequest;
+                message = ex.Message;
                 break;
 
             default:
                 statusCode = HttpStatusCode.InternalServerError;
-                message = "Please re-try again, if the error persist please contact Administrator";
+                message = "Please re-try again, if the error persists please contact Administrator";
                 break;
         }
 
         if (env.IsDevelopment())
         {
-            message += $" | Exception: {exception.Message} | StackTrace: {exception.StackTrace}";
+            message += $" | Exception: {ex.Message} | StackTrace: {ex.StackTrace}";
         }
 
         context.Response.StatusCode = (int)statusCode;
